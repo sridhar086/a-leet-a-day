2ae9b643777e29d76949ab88999a8f99a9868412
Sridhar Ramasamy <sridhar.ramasamy@oracle.com>
2023-10-17 21:18:28 -0700

    stack solution

--- src/main/java/com/sridhar/code/Code.java
+++ src/main/java/com/sridhar/code/Code.java
@@ -1,26 +1,13 @@
 package com.sridhar.code;
 
-import com.sridhar.code.library.ListNode;
-import com.sridhar.code.library.MoveZeroes;
-import com.sridhar.code.library.PalindromeLinkedList;
+import com.sridhar.code.library.*;
 
 public class Code {
 
     public static void main(String[] args) {
 
-        PalindromeLinkedList palindromeLinkedList = new PalindromeLinkedList();
-        ListNode node5 = new ListNode(1);
-        ListNode node4 = new ListNode(2, node5);
-        ListNode node3 = new ListNode(3, node4);
-        ListNode node2 = new ListNode(2, node3);
-        ListNode node1 = new ListNode(1, node2);
-
-        ListNode noded = new ListNode(1);
-        ListNode nodec = new ListNode(2, noded);
-        ListNode nodeb = new ListNode(1, nodec);
-        ListNode nodea = new ListNode(1, nodeb);
-
-        System.out.println(palindromeLinkedList.isPalindrome2(nodea));
+        RemoveStringDuplicates removeDuplicate = new RemoveStringDuplicates();
+        System.out.println(removeDuplicate.removeDuplicates(new String("abbaca")));
 
     }
 
