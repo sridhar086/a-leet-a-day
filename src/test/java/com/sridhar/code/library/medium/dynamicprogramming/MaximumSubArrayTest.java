0c854b2255245d5609e42f6f271ad790f05d5f0e
Sridhar Ramasamy <sridhar.ramasamy@oracle.com>
2023-11-25 09:16:30 -0800

    tests

--- src/test/java/com/sridhar/code/library/medium/dynamicprogramming/MaximumSubArrayTest.java
+++ src/test/java/com/sridhar/code/library/medium/dynamicprogramming/MaximumSubArrayTest.java
@@ -1,2 +1,14 @@
-package com.sridhar.code.library.medium.dynamicprogramming;public class MaximumSubArrayTest {
+package com.sridhar.code.library.medium.dynamicprogramming;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaximumSubArrayTest {
+
+    @Test
+    public void test() {
+        MaximumSubArray maximumSubArray = new MaximumSubArray();
+        int sum = maximumSubArray.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+        Assertions.assertEquals(6, sum);
+    }
 }
